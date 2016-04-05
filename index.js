8d5d9481787f272413b7b975babc66873040f8a8
cassicakes <aguiar.cas@gmail.com>
2016-04-04 19:56:16 -0700

    video homepage working

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-var express = require('express'); //manipulate data
-var app = express();
-var bodyParser = require('body-parser'); //allows us to recieve post data and stores in req.body to use (middlewear)
+var express = require('express');
 var ejsLayouts = require("express-ejs-layouts");
+var app = express();
+var bodyParser = require('body-parser');
 var radioCtrl = require('./controllers/radio'); //NEED HELP WITH THIS
 var request = require('request');
 
-app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/static'));
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(ejsLayouts);
-app.use('radio', radioCtrl); //NEED HELP WITH THIS, ALSO DO I NEEED PARTIALS WITH THIS?
+app.use('radio', radioCtrl); //NEED HELP WITH THIS
 
 app.get('/', function(req, res) {
   res.render('index');
