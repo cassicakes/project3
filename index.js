17693c6d8531bf157826c43b27373f9b15988b0a
cassicakes <aguiar.cas@gmail.com>
2016-04-05 14:32:33 -0700

    styling on pages and added mongo

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ var app = express();
 var bodyParser = require('body-parser');
 var radioCtrl = require('./controllers/radio'); //NEED HELP WITH THIS
 var request = require('request');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/hash-rocket');
+
+var User = require('./models/user')
 
 app.use(express.static(__dirname + '/static'));
 
@@ -16,6 +20,29 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
+app.get('/profile', function (req, res) {
+  res.render('profile');
+});
+
+app.get('/search', function (req, res) {
+  res.render('search');
+});
+
+app.get('/login', function (req, res) {
+  res.render('login');
+});
+
+app.get('/signup', function (req, res) {
+  res.render('signup');
+});
+
+app.get('/secret', function (req, res) {
+  res.render('secret');
+});
+
+app.get('/details', function (req, res) {
+  res.render('details');
+});
 
 
 app.listen(3000);
