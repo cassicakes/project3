b660f77a15b1f2829c4ead848251f90a6a81c8aa
cassicakes <aguiar.cas@gmail.com>
2016-04-05 19:38:29 -0700

    login, flash, signup functional

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var app = express();
 var bodyParser = require('body-parser');
 var radioCtrl = require('./controllers/radio'); //NEED HELP WITH THIS
 var request = require('request');
+var session = require('express-session');
+var flash = require('connect-flash');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/hash-rocket');
 
@@ -13,15 +15,21 @@ app.use(express.static(__dirname + '/static'));
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(session({
+ secret:'3da6f15641a9a688457d114185f91ea907b4f7d5f793770baf5ae',
+ resave: false,
+ saveUninitialized: true
+}));
 app.use(ejsLayouts);
+app.use(flash());
 app.use('radio', radioCtrl); //NEED HELP WITH THIS
 
 app.get('/', function(req, res) {
-  res.render('index');
+  res.render('index', {alerts: req.flash()});
 });
 
 app.get('/profile', function (req, res) {
-  res.render('profile');
+  res.render('profile', {alerts: req.flash()});
 });
 
 app.get('/search', function (req, res) {
@@ -29,11 +37,79 @@ app.get('/search', function (req, res) {
 });
 
 app.get('/login', function (req, res) {
-  res.render('login');
+  res.render('login', {alerts: req.flash()});
+});
+
+app.post('/login', function (req, res) {
+  User.findOne({
+    email: req.body.email, //PUT PW IN AFTER ENCRYPT
+  }, function(err, user) {
+    if (err) {
+      req.flash('danger', "Try again, we suck right now.");
+      res.redirect('/login');
+      return
+    }
+    if (!user) {
+      req.flash('danger', "Could not find user.");
+      res.redirect('/login');
+      return
+    }
+    if (user.password === req.body.password) {
+      req.flash('success', "Music lover logged in.");
+      req.session.user = user;
+      res.redirect('/profile');
+      return
+    } else {
+      req.flash('danger', "Incorrect Password.");
+      res.redirect('/profile');
+      return
+    }
+  })
+});
+
+app.get('/logout', function (req,res) {
+  req.session.destroy();
+  res.redirect('/');
 });
 
 app.get('/signup', function (req, res) {
-  res.render('signup');
+  res.render('signup', {alerts: req.flash()});
+});
+
+app.post('/signup', function (req, res) {
+  if (req.body.password1 !== req.body.password2){
+    req.flash('danger', "Passwords do not match.");
+    res.redirect('/signup');
+    return //if err this ends if statment/ sign up capabilities. 
+  }
+  if (req.body.email == "" ) { //LOOK INTO IMPLENTING CORRECT EMAIL FILTER
+    req.flash('danger', "Please enter a email.");
+    res.redirect('/signup');
+    return
+  }
+  if (req.body.name == "") {
+    req.flash('danger', "Please enter a name.");
+    res.redirect('/signup');
+    return
+  }
+  if (req.body.password1 == ""){
+    req.flash('danger', "Please enter a password.");
+    res.redirect('/signup');
+    return
+  }
+  User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password1
+  },function(err, user){
+    if (err) {
+      req.flash('danger', "Email already exists.");
+      res.redirect('/signup');
+      return
+    }
+    req.flash('success', "Profile Created");
+    res.redirect('/');
+  })
 });
 
 app.get('/secret', function (req, res) {
--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var userSchema = new mongoose.Schema({
   name: String,
-  email: String,
+  email: {type: String, unique: true},
   password: String
 
 });
